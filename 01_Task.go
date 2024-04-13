47e88cc1aefadb1db223b2e2c1a7204465fdb2ca
Keerthi raja <105999990+22Raja@users.noreply.github.com>
2024-04-13 17:02:39 +0530

    Add files via upload
    A simple echo server

---
+++ 01_Task.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	//"net/http"
+	"main"
+	 
+
+	//"github.com/labstack/echo/v4"
+)
+
+/*func handler(c echo.Context) error {
+	name := c.QueryParam("name")
+	return c.String(http.StatusOK, fmt.Sprintf(" Hi %s sir, I hope ypu had a great day", name))
+}*/
+Raja()
+
+
+/*func main() {
+	e := echo.New()
+	e.GET("/raja", handler)
+
+	e.Start(":3000")
+}*/
