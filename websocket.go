7614c278960419aa4b024c89948134b72cdd915a
Keerthi raja <105999990+22Raja@users.noreply.github.com>
2024-05-15 13:15:01 +0530

    Update websocket.go
    Here we added Room, So each person can create a unique clan and be part of it.

--- websocket.go
+++ websocket.go
@@ -8,14 +8,20 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-var clients = make(map[*websocket.Conn]bool) // connected clients
-var broadcast = make(chan Message)           // broadcast channel
+// clients keeps track of the room each connection belongs to
+var clients = make(map[*websocket.Conn]string)
 
+// broadcast is a channel for sending messages to specific rooms
+var broadcast = make(chan Message)
+
+// Message defines the structure for a user's message along with their room
 type Message struct {
 	Username string
 	Message  string
+	Room     string
 }
 
+// upgrader is used to upgrade the HTTP connection to a WebSocket connection
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
 		return true // Allow connection from any origin
@@ -33,56 +39,73 @@ func main() {
 }
 
 func handleConnections(w http.ResponseWriter, r *http.Request) {
+	// Get username and room from the query parameters
 	username := r.URL.Query().Get("username")
+	room := r.URL.Query().Get("room")
 
-	// Upgrade initial GET request to a websocket
+	// Upgrade the HTTP server connection to the WebSocket protocol
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println("Error upgrading to websocket:", err)
 		return
 	}
-	defer conn.Close()
+	if username == "" && room == "" {
+		conn.WriteMessage(websocket.TextMessage, []byte("Username and Room Name Not specified."))
+		log.Println("Username not specified")
+		conn.Close()
 
-	// Register client
-	clients[conn] = true
-	if len(username) == 0 {
-		username = "anonymous"
+		return
+	}
+	if room == "" {
+
+		conn.WriteMessage(websocket.TextMessage, []byte("Room Not specified"))
+
+		log.Println("Room not specified")
+		conn.Close()
+
+		return
 
 	}
-	fmt.Print(username)
+	if username == "" {
+		conn.WriteMessage(websocket.TextMessage, []byte("Username Not specified."))
+		log.Println("Username not specified")
+		conn.Close()
+
+		return
+	}
+	defer conn.Close()
+
+	// Register client and its room
+	clients[conn] = room
+	fmt.Printf("%s joined %s\n", username, room)
 
 	for {
-		//var msg Message
-		// Read message from browser
 		_, message, err := conn.ReadMessage()
 		if err != nil {
 			log.Printf("error: %v", err)
 			delete(clients, conn)
 			break
 		}
-		// Convert the message from bytes to a string
-		textMessage := string(message)
 
-		// Assume the text message contains simple text that can be directly handled
-
-		broadcast <- Message{Username: username, Message: textMessage}
+		broadcast <- Message{Username: username, Message: string(message), Room: room}
 	}
 }
 
 func handleMessages() {
 	for {
-		// Grab the next message from the broadcast channel
 		msg := <-broadcast
 
-		log.Printf("%s: %s\n", msg.Username, msg.Message)
+		log.Printf("%s in %s: %s\n", msg.Username, msg.Room, msg.Message)
 
-		// Send it out to every client that is currently connected
-		for client := range clients {
-			err := client.WriteMessage(websocket.TextMessage, []byte("your Message is received"))
-			if err != nil {
-				log.Printf("error: %v", err)
-				client.Close()
-				delete(clients, client)
+		// Send the message to all clients in the same room
+		for client, room := range clients {
+			if room == msg.Room {
+				err := client.WriteMessage(websocket.TextMessage, []byte(msg.Username+": "+msg.Message))
+				if err != nil {
+					log.Printf("Websocket error: %v", err)
+					client.Close()
+					delete(clients, client)
+				}
 			}
 		}
 	}
